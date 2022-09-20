46f191cf9fdf2e6cb310185d4540922a06bc8064
Irmak Erdem <irmakerdem9@gmail.com>
2022-09-20 16:22:51 -0600

    Add link to nav bar

--- src/Components/Nav/nav.js
+++ src/Components/Nav/nav.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Nav.scss';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
   return (
     <>
+    <Link to='/'>
       <h1 className='header'>NYT Reader</h1>
+    </Link>
     </>
   )
 }
