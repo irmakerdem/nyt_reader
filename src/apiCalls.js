0a28c08f7e71a892abd22775f4ed90b93b9835f8
Irmak Erdem <irmakerdem9@gmail.com>
2022-09-19 19:03:39 -0600

    Add async await syntax

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,3 +1,5 @@
+// import { useEffect } from 'react';
+
 export const getArticles = () => {
   return fetch('https://api.nytimes.com/svc/topstories/v2/home.json?api-key=xUoI1JmR6Bufafw2f3ZRJOQZUbAcO5L9')
     .then(response => {
@@ -8,3 +10,13 @@ export const getArticles = () => {
       }
     })
 }
+
+// useEffect(() => {
+//   const getArticles = async () => {
+//     const data = await fetch('https://api.nytimes.com/svc/topstories/v2/home.json?api-key=xUoI1JmR6Bufafw2f3ZRJOQZUbAcO5L9')
+//   }
+//   getArticles()
+//   .catch(console.error)
+// }, [])
+
+// https://devtrium.com/posts/async-functions-useeffect
